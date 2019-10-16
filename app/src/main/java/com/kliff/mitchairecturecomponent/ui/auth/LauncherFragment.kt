c129aa2ca071881fa75e8bb6853d6365a147171a
pankdev09 <pankawat9@gmail.com>
2019-10-16 17:41:33 +0530

    Initial commit

---
+++ app/src/main/java/com/kliff/mitchairecturecomponent/ui/auth/LauncherFragment.kt
@@ -0,0 +1,43 @@
+package com.kliff.mitchairecturecomponent.ui.auth
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+
+import com.kliff.mitchairecturecomponent.R
+import kotlinx.android.synthetic.main.fragment_launcher.*
+
+
+class LauncherFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_launcher, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        register.setOnClickListener{
+            navRegistration()
+        }
+        login.setOnClickListener{
+            navLogin()
+        }
+    }
+
+    private fun navLogin() {
+        findNavController().navigate(R.id.action_launcherFragment_to_loginFragment)
+    }
+
+    private fun navRegistration() {
+        findNavController().navigate(R.id.action_launcherFragment_to_registerFragment)
+    }
+
+}
